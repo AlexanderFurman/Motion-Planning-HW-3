dbf02d69f61c74c0aef4200189fb1d6e797321e1
tomagami1990 <58557948+tomagami1990@users.noreply.github.com>
2023-01-14 14:08:21 +0200

    robot update

--- Robot.py
+++ Robot.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy.core.fromnumeric import size
 from shapely.geometry import Point, LineString
-
+#import MapEnvironment
 class Robot(object):
     
     def __init__(self):
@@ -25,8 +25,15 @@ class Robot(object):
         @param next_config Next configuration.
         '''
         # TODO: Task 2.2
+        # Create two LineString objects
+        line1 = LineString(self.compute_forward_kinematics(prev_config))
+        line2 = LineString(self.compute_forward_kinematics(next_config))
 
-        pass
+        # Calculate the Euclidean distance between the two LineStrings
+        distance = line1.distance(line2)
+        print(distance)
+        return distance
+        #pass
 
     def compute_forward_kinematics(self, given_config):
         '''
@@ -34,8 +41,24 @@ class Robot(object):
         @param given_config Given configuration.
         '''
         # TODO: Task 2.2
-
-        pass
+        # Create a list to store the positions of each link
+        link_positions = []
+        # Define the origin position
+        origin = np.array([0.0, 0.0])
+        # Compute the position of each link
+        position = origin
+        for i in range(len(given_config)):
+            angle=given_config[i]
+            if i > 0:
+                angle = sum(given_config[0:i+1]) # in case of relative angle
+            # Compute the position of the current link
+            position += np.array([self.links[i] * np.cos(angle),
+                                  self.links[i] * np.sin(angle)])
+            # Append the position to the list of link positions
+            link_positions.append(position.copy())
+        # Return the list of link positions
+        return np.asarray(link_positions)
+        #pass
 
     def compute_ee_angle(self, given_config):
         '''
@@ -67,6 +90,36 @@ class Robot(object):
         @param robot_positions Given links positions.
         '''
         # TODO: Task 2.2
+        line = LineString(robot_positions)
+        return line.is_simple
+        # # Iterate over all links
+        # for i in range(len(robot_positions) - 1):
+        #     for j in range(i + 1, len(robot_positions)):
+        #         segment_1 = robot_positions[i], robot_positions[i + 1]
+        #         segment_2 = robot_positions[j], robot_positions[j + 1]
+        #         if self.segments_intersect(segment_1, segment_2):
+        #             #print("Self-collision detected between links", i, "and", j)
+        #             return False
+        # return True
+        #pass
 
-        pass
-    
+    # def segments_intersect(self,segment_1, segment_2):
+    #     # # Determine the intersection point of the two segments
+    #     # x1, y1 = segment_1[0]
+    #     # x2, y2 = segment_1[1]
+    #     # x3, y3 = segment_2[0]
+    #     # x4, y4 = segment_2[1]
+    #     # denominator = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4))
+    #     # if denominator == 0:
+    #     #     return False
+    #     # else:
+    #     #     return True
+    #     # Define two line segments
+    #     line1 = LineString(segment_1)
+    #     line2 = LineString(segment_2)
+    #
+    #     # Check if the lines intersect
+    #     if line1.intersects(line2):
+    #         return True
+    #     else:
+    #         return False
